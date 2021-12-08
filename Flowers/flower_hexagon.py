87332579a156519a24c57080c7ea276d0da4f80a
Chi Truong <vtchitruong@gmail.com>
2021-12-08 10:56:34 +0700

    Create the flower of hexagons

---
+++ Flowers/flower_hexagon.py
@@ -0,0 +1,15 @@
+import turtle
+
+s = turtle.getscreen() # init screen
+t = turtle.Turtle() # init pen
+
+t.width(3) # width() is an alias for pensize()
+t.speed(0) # super fast
+t.color('cyan') # lime green
+turtle.bgcolor('black') 
+
+for i in range(18):
+    t.circle(180, steps=6) # draw hexagon
+    t.right(100) # turn right by 100 degrees
+
+t.screen.mainloop() # pause for viewing
