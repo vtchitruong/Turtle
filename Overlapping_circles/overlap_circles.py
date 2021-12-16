54b637230af7db5489a5aa4a44e30041b70dcf7a
Chi Truong <vtchitruong@gmail.com>
2021-12-16 22:24:02 +0700

    Overlapping circles

--- Overlapping_circles/overlap_circles.py
+++ Overlapping_circles/overlap_circles.py
@@ -1,14 +1,14 @@
 import turtle
 
 s = turtle.getscreen() # init screen
-t = turtle.Turtle() # init pen
+p = turtle.Turtle() # init pen
 
-t.width(3) # width() is an alias for pensize()
-t.speed(0) # super fast
-t.color('#32cd32') # lime green
+p.width(3) # width() is an alias for pensize()
+p.speed(0) # super fast
+p.color('#32cd32') # lime green
 
 for _ in range(50):
-    t.circle(200) # radius
-    t.right(50) # 50 degrees to the right
+    p.circle(200) # radius
+    p.right(50) # 50 degrees to the right
 
-t.screen.mainloop() # pause for viewing
+p.screen.mainloop() # pause for viewing
