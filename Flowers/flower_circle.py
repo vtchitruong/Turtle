601f41586382b7599ac95e3ae3968d813200a72d
Chi Truong <vtchitruong@gmail.com>
2021-12-16 22:14:08 +0700

    An Asteraceae or Gerbera-like flower

---
+++ Flowers/flower_circle.py
@@ -0,0 +1,20 @@
+import turtle
+
+s = turtle.getscreen() # init screen
+p = turtle.Turtle() # init pen
+
+p.width(3) # width() is an alias for pensize()
+p.speed(0) # super fast
+p.color('#ffeb0f') # yellow
+
+for _ in range(36):
+    # draw the lower stroke
+    p.circle(radius=200, extent=90)
+    
+    # draw an upper stroke
+    p.left(100)    
+    p.circle(radius=200, extent=90)
+
+    p.left(10)
+
+p.screen.mainloop() # pause for viewing
