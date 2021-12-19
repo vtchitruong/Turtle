bf8413ca51aae661de01a77be056f025b8f24587
Chi Truong <vtchitruong@gmail.com>
2021-12-19 07:50:35 +0700

    Spiral made of  hexagons with 6 colors

--- Spiral/spiral_rainbow.py
+++ Spiral/spiral_rainbow.py
@@ -4,14 +4,14 @@ colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 s = turtle.getscreen() # init screen
 turtle.Screen().bgcolor('black')
 
-t = turtle.Turtle() # init pen
+p = turtle.Turtle() # init pen
 
-t.speed(0) # super fast
+p.speed(0) # super fast
 
 for x in range(360):
-    t.pencolor(colors[x % 6])
-    t.width(x // 100 + 1)
-    t.forward(x)
-    t.left(59)
+    p.pencolor(colors[x % 6]) # set 6 different colors to 6 edges
+    p.width(x // 100) # set pensize from thin to thick   
+    p.forward(x) # draw 1 edge
+    p.left(61) # turn left by 60 degrees for spiral effect
 
-t.screen.mainloop() # pause for viewing
+p.screen.mainloop() # pause for viewing
