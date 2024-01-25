727d7cb9b076ba05967d53ebca11b0a5a9e29bc5
Vo Thach Chi Truong <vtchitruong@gmail.com>
2024-01-25 21:49:48 +0700

    Modify some code.

--- Flowers/flower_circle.py
+++ Flowers/flower_circle.py
@@ -1,20 +1,21 @@
 import turtle
 
-s = turtle.getscreen() # init screen
-p = turtle.Turtle() # init pen
+if __name__ == '__main__':
+    s = turtle.getscreen() # init screen
+    p = turtle.Turtle() # init pen
 
-p.width(3) # width() is an alias for pensize()
-p.speed(0) # super fast
-p.color('#ffeb0f') # yellow
+    p.width(3) # width() is an alias for pensize()
+    p.speed(0) # super fast
+    p.color('#ffeb0f') # yellow
 
-for _ in range(36):
-    # draw the lower stroke
-    p.circle(radius=200, extent=90) # a 90 degree arc
-    
-    # draw an upper stroke
-    p.left(100)    
-    p.circle(radius=200, extent=90) # a 90 degree arc
+    for _ in range(36):
+        # draw the lower stroke
+        p.circle(radius=200, extent=90) # a 90 degree arc
+        
+        # draw an upper stroke
+        p.left(100)    
+        p.circle(radius=200, extent=90) # a 90 degree arc
 
-    p.left(10)
+        p.left(10)
 
-p.screen.mainloop() # pause for viewing
+    p.screen.mainloop() # pause for viewing
