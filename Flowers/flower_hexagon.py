b6eba991b6ddb095382427f49299eaa01052ab9d
Vo Thach Chi Truong <vtchitruong@gmail.com>
2024-01-26 08:18:00 +0700

    Modify some code.

--- Flowers/flower_hexagon.py
+++ Flowers/flower_hexagon.py
@@ -1,15 +1,16 @@
 import turtle
 
-s = turtle.getscreen() # init screen
-t = turtle.Turtle() # init pen
+if __name__ == '__main__':
+    s = turtle.getscreen() # init screen
+    t = turtle.Turtle() # init pen
 
-t.width(3) # width() is an alias for pensize()
-t.speed(0) # super fast
-t.color('cyan')
-turtle.bgcolor('black') 
+    t.width(3) # width() is an alias for pensize()
+    t.speed(0) # super fast
+    t.color('cyan')
+    turtle.bgcolor('black') 
 
-for i in range(18):
-    t.circle(180, steps=6) # draw hexagon
-    t.right(100) # turn right by 100 degrees
+    for i in range(18):
+        t.circle(180, steps=6) # draw hexagon
+        t.right(100) # turn right by 100 degrees
 
-t.screen.mainloop() # pause for viewing
+    t.screen.mainloop() # pause for viewing
